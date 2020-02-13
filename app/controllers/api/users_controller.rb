838b350e9a1fccc3ba69728d31c191263d65485d
abdellani <abdellani@gmail.com>
2020-02-13 16:19:04 +0100

    Add create User

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::UsersController < ApplicationController
+  def create
+    return if check_password_confiramtion
+    user=User.new(user_params)
+    
+    if user && user.save
+      token = generate_new_jwt_token(user)
+      render json: {status: 200, token:token}
+    else
+      render json: { status: 422, message: user.errors.messages}, status: 422
+    end
+  end
+
+  private
+  def check_password_confiramtion
+    render json: {status: 400, error:"Password confirmation is required!"}, status: 400 if params[:user][:password_confirmation].nil? 
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:name,:password,:password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :sessions, only: [:create,:destroy]
+    resources :users, only: [:create]
   end
   root "pages#index"
 end
