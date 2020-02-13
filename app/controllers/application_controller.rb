4bca86a9580bd13fa6180659e77ccac0c8ebb425
abdellani <abdellani@gmail.com>
2020-02-13 16:16:35 +0100

    define generate_new_jwt_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 require_relative "../../lib/authentication/helper_methods.rb"
 class ApplicationController < ActionController::API
   include Authentication::HelperMethods
+  protected
+  def generate_new_jwt_token(user)
+    payload= {
+      id:user.id,
+      access_token:user.access_token
+    }
+    #TODO Add expiration data
+    token = JWT.encode payload, ENV['JWT_PASSWORD'], 'HS256'
+  end
 end
