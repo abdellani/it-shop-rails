7346d3613518f11b8f0f53364c6a43fc95db31fd
abdellani <abdellani@gmail.com>
2020-02-13 19:48:42 +0100

    define login/signup paths

--- app/javascript/packs/src/application.jsx
+++ app/javascript/packs/src/application.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import LoginPage from "./containers/LoginPage"
-import SignupPage from "./containers/SignupPage"
-import GuestRoute from "./containers/GuestRoute"
-const Application=()=>
-<div>
-  <GuestRoute Component={LoginPage}/>
-  <GuestRoute Component={SignupPage}/>
-  
-  {/* <SignupPage /> */}
-  {/* <LoginPage /> */}
-</div> 
+import React from "react";
+import LoginPage from "./containers/LoginPage";
+import SignupPage from "./containers/SignupPage";
+import GuestRoute from "./containers/GuestRoute";
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+
+const Index = () => <div>index page</div>;
+const Application = () => (
+  <Router>
+    <div>
+      <Link to="login">login</Link>
+      <Link to="signup">signup</Link>
+      <Link to="/">home</Link>
+    </div>
+    <Switch>
+      <Route path="/login">
+        <GuestRoute Component={LoginPage} />
+      </Route>
+      <Route path="/signup">
+        <GuestRoute Component={SignupPage} />
+      </Route>
+      <Route>
+        <Index />
+      </Route>
+    </Switch>
+    {/* <SignupPage /> */}
+    {/* <LoginPage /> */}
+  </Router>
+);
 export default Application;
--- app/javascript/packs/src/containers/GuestRoute.jsx
+++ app/javascript/packs/src/containers/GuestRoute.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import {Redirect} from "react-router-dom";
+
 const GuestRoute = class extends React.Component {
     constructor() {
       super();
     }
     render() {
       let {Component}=this.props
-      if (this.props.token) return <div>you are not supposed to access to this page !</div>;
+      if (this.props.token) return <Redirect to="/" />;
       else return <Component/>;
     }
   };
