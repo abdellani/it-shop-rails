9eca11eace81235ef23a994d71505fac88a44dea
abdellani <abdellani@gmail.com>
2020-02-25 10:48:30 +0100

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :photos, only: [:show]
-    resources :products, only: [:index, :create, :show], module: :products do
-      resources :comments, only: [:index, :create]
-      resources :visits, only: [:index]
+    resources :products, only: [:index, :create, :show] do
+      scope module: :products do
+        resources :comments, only: [:index, :create]
+        resources :visits, only: [:index]
+      end
     end
     resources :sessions, only: [:create, :destroy]
     resources :users, only: [:create, :show]
     resources :comments, only: [:destroy]
     namespace :loggedin do
-      resources :products, only: [:index,:update, :destroy]
-      resources :comments, only: [ :update]
+      resources :products, only: [:index, :update, :destroy]
+      resources :comments, only: [:update]
       get "notifications/count", to: "notifications#count"
       resources :notifications, only: [:index]
-      resources :orders, only: [:index,:create]
-      get 'requests', to: 'orders#requests_index'
+      resources :orders, only: [:index, :create]
+      get "requests", to: "orders#requests_index"
     end
   end
   root "pages#index"
